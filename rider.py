c71ca0de462e94d9702e3d64c372e9be18d933e7
haqfarzan <farzan_97@hotmail.com>
2016-02-04 23:30:34 -0500

    Update

--- rider.py
+++ rider.py
@@ -17,5 +17,42 @@ SATISFIED = "satisfied"
 
 
 class Rider:
-    # TODO
-    pass
+    def __init__(self, identifier, origin, destination, status, patience):
+        """Initialize an rider.
+
+        @type self: Rider
+        @type identifier: int
+        @type origin: str
+        @type destination: str
+        @type status: str
+        @type patience: int
+        @rtype: None
+        """
+        self.identifier = identifier
+        self.origin = origin
+        self.destination = destination
+        self.status = status
+        self.patience = patience
+
+    def __str__(self):
+        """Return a string representation.
+
+        @type self: Rider
+        @rtype: str
+        """
+        return "(ID:{}, Origin:{}, Destination:{}, Status:{}, Patience:{})"\
+            .format(self.identifier, self.origin, self.destination,\
+                    self.status, self.patience)
+
+    def __eq__(self, other):
+        """Return True if self equals other, and false otherwise.
+
+        @type self: Rider
+        @type other: Rider | other
+        @rtype: bool
+        """
+        return (self.identifier == other.identifier)\
+               and (self.origin == other.origin)\
+               and (self.destination == other.destination)\
+               and (self.status == other.status)\
+               and (self.patience == other.patience)
